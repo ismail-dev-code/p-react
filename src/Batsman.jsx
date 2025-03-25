18eed332939d4439bfd7a3a2f9b2d4b7ebd8af11
ismail-dev-code <ismail.online0385678@gmail.com>
2025-03-25 14:49:48 +0600

    total fours count added while click the btn

--- src/Batsman.jsx
+++ src/Batsman.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function Batsman() {
   const [runs, setRuns] = useState(0);
   const [sixes, setSixes] = useState(0);
+  const [fours, setFours] = useState(0);
   const handleSingles = () => {
     const updatedSingles = runs + 1;
     setRuns(updatedSingles);
@@ -10,6 +11,8 @@ export default function Batsman() {
 
   const handleFour = () => {
     const updatedFour = runs + 4;
+    const fourCount = fours + 1;
+    setFours(fourCount);
     setRuns(updatedFour);
   };
   const handleSix = () => {
@@ -22,7 +25,10 @@ export default function Batsman() {
     <div className="card">
       {runs > 50 && <p>You Score 50</p>}
       <h3>Player: Bangladesh</h3>
-      <p><small>total sixes: {sixes}</small></p>
+      <p>
+        <small>total sixes: {sixes}</small>
+      </p>
+      <p><small>total fours: {fours}</small></p>
       <h3>Score: {runs}</h3>
       <button onClick={handleSingles}>1 Run</button>
       <button onClick={handleFour}>4 Run</button>
